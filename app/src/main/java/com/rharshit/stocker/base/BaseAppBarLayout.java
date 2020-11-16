9c6c728c1fe6bf9a7794b0135731e3495caa3565
Harshit Raghuvanshi <rharshit98@gmail.com>
2020-11-16 13:46:51 +0530

    Add BaseAppBarLayout

---
+++ app/src/main/java/com/rharshit/stocker/base/BaseAppBarLayout.java
@@ -0,0 +1,23 @@
+package com.rharshit.stocker.base;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+public class BaseAppBarLayout extends AppBarLayout {
+    public BaseAppBarLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public BaseAppBarLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseAppBarLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
