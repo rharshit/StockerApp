8a80e63554d7945b2bea7d17923aefae4873a977
Harshit Raghuvanshi <rharshit98@gmail.com>
2020-11-20 18:49:50 +0530

    Add base async task

--- app/src/main/java/com/rharshit/stocker/base/ui/BaseViewModel.java
+++ app/src/main/java/com/rharshit/stocker/base/ui/BaseViewModel.java
@@ -2,21 +2,14 @@ package com.rharshit.stocker.base.ui;
 
 import android.os.AsyncTask;
 
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class BaseViewModel extends ViewModel {
 
-    MutableLiveData<Map<Integer, AsyncTask>> blockedUiTasks;
-    MutableLiveData<Map<Integer, AsyncTask>> nonBlockedUiTasks;
     private BaseAppCompatActivity activity;
 
     public BaseViewModel() {
-        blockedUiTasks = new MutableLiveData<>(new HashMap<>());
-        nonBlockedUiTasks = new MutableLiveData<>(new HashMap<>());
+
     }
 
     public BaseAppCompatActivity getActivity() {
@@ -25,5 +18,51 @@ public class BaseViewModel extends ViewModel {
 
     public void setActivity(BaseAppCompatActivity activity) {
         this.activity = activity;
+    }
+
+    public abstract class BaseAsyncTask<X, Y, Z> extends AsyncTask<X, Y, Z> {
+
+        private final boolean isUiBlocked;
+        private final String taskName;
+
+        public BaseAsyncTask(String taskName, boolean isUiBlocked) {
+            this.isUiBlocked = isUiBlocked;
+            this.taskName = taskName;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (this.isUiBlocked) {
+                activity.addLoading(this.taskName);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Z z) {
+            super.onPostExecute(z);
+            if (this.isUiBlocked) {
+                activity.removeLoading(this.taskName);
+            }
+        }
+
+        @Override
+        protected void onCancelled(Z z) {
+            super.onCancelled(z);
+            if (this.isUiBlocked) {
+                activity.removeLoading(this.taskName);
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            if (this.isUiBlocked) {
+                activity.removeLoading(this.taskName);
+            }
+        }
+
+        @Override
+        protected abstract Z doInBackground(X... xes);
     }
 }
