7e877587e270b6a09bd1769256d5f4d9f1a3e05f
Harshit Raghuvanshi <rharshit98@gmail.com>
2020-12-19 14:42:55 +0530

    MarketStack: Init API keys and endpoints

--- app/src/main/java/com/rharshit/stocker/constant/APIConstants.java
+++ app/src/main/java/com/rharshit/stocker/constant/APIConstants.java
@@ -1,5 +1,7 @@
 package com.rharshit.stocker.constant;
 
+import com.rharshit.stocker.BuildConfig;
+
 public abstract class APIConstants {
 
     private APIConstants() {
@@ -10,10 +12,16 @@ public abstract class APIConstants {
      * Base URLs
      */
     public static final String BASE_URL_CHUCK = "https://api.chucknorris.io/";
+    public static final String BASE_URL_MARKETSTACK = "https://api.marketstack.com/v1";
 
     /**
      * endpoints
      */
 
     public static final String GET_CHUCK_NORRIS_JOKES = "/jokes/random";
+
+    /**
+     * Kays
+     */
+    public static final String MARKETSTACK_API_KEY = BuildConfig.MARKETSTACK_API_KEY;
 }
